451979bd61aad38a73921ff3568f410cdc3c4ce1
Melios025 <84947414+Melios025@users.noreply.github.com>
2022-12-26 23:18:03 +0700

    update homepage

--- AppWeather/AppWeather.Android/obj/Debug/110/android/environment.x86_64.s
+++ AppWeather/AppWeather.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"32631885-1de6-4c66-ad33-f22205bafc60"
+	.asciz	"ee02f6e9-aa10-4090-9fc1-a39d1efabb05"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
